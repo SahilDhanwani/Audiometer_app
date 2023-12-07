a8cf3cebf50d585f9a4dbcadab52afa2ff84b98d
SahilDhanwani <sahildhanwani291203@gmail.com>
2023-12-08 02:08:08 +0530

    Added Dependencies for Background Check

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/pages/background_noise.dart';
 import 'package:flutter_application/pages/home_page.dart';
 import 'package:flutter_application/pages/student_signup.dart';
 import 'package:flutter_application/pages/student_login.dart';
 import 'package:flutter_application/pages/student_dashboard.dart';
 import 'package:flutter_application/utils/routes.dart';
 import 'package:flutter_application/widgets/themes.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Permission.microphone.request();
   runApp(const MyApp());
 }
 
@@ -20,12 +24,13 @@ class MyApp extends StatelessWidget {
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
-      initialRoute: MyRoutes.homeRoute,
+      initialRoute: MyRoutes.backgroundNoise,
       routes: {
         MyRoutes.homeRoute: (context) => const HomePage(),
         MyRoutes.studentLoginRoute: (context) => const student_login(),
         MyRoutes.studentDashboard: (context) => const StudentDashboard(),
         MyRoutes.studentSignupRoute: (context) => const StudentSignup(),
+        MyRoutes.backgroundNoise: (context) => const BackgroundNoise(),
       },
     );
   }
